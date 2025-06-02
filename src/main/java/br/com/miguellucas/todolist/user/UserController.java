1d35b350fb9913c49aade0a23998a8fe3f6ada90
Miguel Lucas <lucasmiguel646@gmail.com>
2025-06-01 22:18:04 -0300

    implementação de criptografia das senhas
    adicionada bcrypt e logica pra criptografia

--- src/main/java/br/com/miguellucas/todolist/user/UserController.java
+++ src/main/java/br/com/miguellucas/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.miguellucas.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,9 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe"); //Retorn status 400 se o usuario estiver cadastrado
 
         }
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
